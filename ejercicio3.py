151d0586826e20db5a1263f88fdb3544594c31ea
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-04-23 14:16:24 -0600

    mis ejercicios

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+velocidad = int(input("Velocidad en km/h: "))
+if (velocidad > 120)
+    print("Reduzca la velocidad")
+else:
+   (velocidad < 120)
+    print("Continue a esa velocidad")
+
+
---
+++ ejercico2.py
@@ -0,0 +1,14 @@
+#Detectar si un usuario esta inactivo por mas de 30 dias
+
+import datetime as dt
+
+fecha_inicio_sesion = input("Ultimo inicio de sesion en formato YYYY-MM-DD: ")
+fecha_inicio_sesion = dt.datetime.strptime(fecha_inicio_sesion. "%Y-%m-%d")
+
+fecha_actual = dt.datetime.now()
+
+contar_dias= (fecha_actual - fecha_inicio_sesion).days
+if contar_dias > 30:
+    print("Inactivo por mas de 30 dias")
+else:
+    print("Estamos bien.")
